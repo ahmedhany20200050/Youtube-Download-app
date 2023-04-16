e744b1baebeea77595c1d4f3f8d98305a42d8fa2
ahmed <ahmedhany20200050@gmail.com>
2023-04-16 19:52:26 +0200

    can download mp3 and open download folder 2

--- android/app/src/main/kotlin/com/example/youtube_test/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/youtube_test/MainActivity.kt
@@ -1,6 +1,51 @@
 package com.example.youtube_test
 
+import android.content.Intent
+import android.media.MediaScannerConnection
+import io.flutter.plugins.GeneratedPluginRegistrant
+import android.os.Bundle;
+import io.flutter.plugin.common.MethodChannel
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import java.io.File
+import android.net.Uri
+import android.provider.DocumentsContract
 
 class MainActivity: FlutterActivity() {
+    companion object {
+        const val channel="com.flutter.epic/mp3"
+    }
+    fun scanning(path: String){
+        val file = File(path)
+        MediaScannerConnection.scanFile(context, arrayOf(file.toString()), null, null)
+    }
+    fun openFolder(folderPath: String) {
+        val intent = Intent(Intent.ACTION_VIEW)
+        val uri = Uri.parse("$folderPath")
+        intent.setDataAndType(uri, "*/*")
+        startActivity(intent)
+    }
+
+    fun update(filePath: String){
+        val file = File(filePath)
+        MediaScannerConnection.scanFile(context, arrayOf(file.toString()),
+            null, null)
+    }
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger,channel).setMethodCallHandler{
+            call,result->
+            if(call.method=="open"){
+                print("niggaaaaaaaaaaaa")
+                call.argument<String>("path")?.let { openFolder(it)
+                print(it)}
+                result.success("hi from java nigga")
+            }else if(call.method=="update"){
+                call.argument<String>("update")?.let {update(it) }
+
+            }
+        }
+    }
+
 }
--- lib/download_screen.dart
+++ lib/download_screen.dart
@@ -3,11 +3,13 @@ import 'dart:io';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:image_gallery_saver/image_gallery_saver.dart';
+import 'package:open_file_plus/open_file_plus.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:youtube_explode_dart/youtube_explode_dart.dart';
 import 'package:youtube_test/VideoInfo.dart';
 import 'package:youtube_test/list_item.dart';
+import 'package:flutter/services.dart';
 
 class DownloadScreen extends StatefulWidget {
   const DownloadScreen({Key? key}) : super(key: key);
@@ -17,20 +19,18 @@ class DownloadScreen extends StatefulWidget {
 }
 
 class _DownloadScreenState extends State<DownloadScreen> {
+  static const platform =MethodChannel("com.flutter.epic/mp3");
   List<VideoInfo> videosInfo=[];
   List<ListItem> listItems=[];
   final TextEditingController urlTextController = TextEditingController();
-  VideoInfo currentVideoInfo= VideoInfo();
   double? progress = 0.0;
   bool stop=false;
   String currentlyDownloading="";
 
-
-  //redundant data
-  //todo: to be removed
-  bool downloading = false;
-  bool submitted = false;
   String format = ".mp4";
+  String currentButtonFormat=".mp4";
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -62,7 +62,7 @@ class _DownloadScreenState extends State<DownloadScreen> {
                 Expanded(
                   flex: 1,
                   child: DropdownButton<String>(
-                    value: currentVideoInfo.format,
+                    value: currentButtonFormat,
                     items: <String>['.mp4', '.mp3'].map((String value) {
                       return DropdownMenuItem<String>(
                         value: value,
@@ -71,7 +71,10 @@ class _DownloadScreenState extends State<DownloadScreen> {
                     }).toList(),
                     onChanged: (newFormat) {
                       setState(() {
-                        currentVideoInfo.format=newFormat!;
+                        if (kDebugMode) {
+                          print("my new format $newFormat");
+                        }
+                        currentButtonFormat=newFormat!;
                       });
 
                     },
@@ -125,6 +128,16 @@ class _DownloadScreenState extends State<DownloadScreen> {
             flex: 1,
             child:  Text("$currentlyDownloading \nprogress: ${(progress! >= 1) ? "100.0" : (progress! * 100).toStringAsFixed(2)}%"),
           ),
+          Expanded(
+            flex: 1,
+            child:  TextButton.icon(
+              onPressed: () {
+                openDownloadFolder();
+              },
+              icon: const Icon(Icons.download),
+              label: const Text("Open Download Folder"),
+            ),
+          ),
         ],
       ),
     );
@@ -139,19 +152,21 @@ class _DownloadScreenState extends State<DownloadScreen> {
 
   //1-info
   Future<void> getInfo(String url) async {
-
+    String currentFormat=currentButtonFormat;
     if (url.contains("list")) {
       var yt = YoutubeExplode();
       // Get playlist metadata.
       var playlist = await yt.playlists.get(url);
       var vv=yt.playlists.getVideos(playlist.id);
       await for (Video v in vv) {
-        currentVideoInfo.video = v;
         var aa=VideoInfo();
         aa.video=v;
-        aa.format=currentVideoInfo.format.toString();
+        aa.format=currentFormat;
         var rr=await yt.videos.streamsClient.getManifest(v.id);
         aa.size=rr.muxed.withHighestBitrate().size.totalMegaBytes;
+        if(aa.format==".mp3"){
+          aa.size=rr.audioOnly.withHighestBitrate().size.totalMegaBytes;
+        }
         //give it a special id (to be needed )
         for(int i=0;i<10000;i++){
           bool ok=true;
@@ -176,12 +191,14 @@ class _DownloadScreenState extends State<DownloadScreen> {
     }else{
       var youtubeInfo = YoutubeExplode();
       Video video = await youtubeInfo.videos.get(url);
-      currentVideoInfo.video = video;
       var aa=VideoInfo();
       aa.video=video;
-      aa.format=currentVideoInfo.format.toString();
+      aa.format=currentFormat;
       var rr=await youtubeInfo.videos.streamsClient.getManifest(video.id);
       aa.size=rr.muxed.withHighestBitrate().size.totalMegaBytes;
+      if(aa.format==".mp3"){
+        aa.size=rr.audioOnly.withHighestBitrate().size.totalMegaBytes;
+      }
       //give it a special id
       for(int i=0;i<10000;i++){
         bool ok=true;
@@ -262,44 +279,44 @@ class _DownloadScreenState extends State<DownloadScreen> {
         var youtubeExplode = YoutubeExplode();
         //get video metadata
         var video = await youtubeExplode.videos.get(videosInfo.first.video.id);
-
-
-
-        setState(() {
-          videosInfo.removeAt(0);
-          currentlyDownloading=video.title;
-          createListOfVideos("download function");
-        });
-        if (kDebugMode) {
-          print(video.title);
-        }
-        var manifest = await youtubeExplode.videos.streamsClient.getManifest(video.id);
-        var audio = manifest.muxed.withHighestBitrate();
-        var audioStream = youtubeExplode.videos.streamsClient.get(audio);
-
-        //time to create file
-        File file;
-        int copyNumber=0;
-        String fileNameAndDirectory;
-        while(true){
-          fileNameAndDirectory='$path/${video.title}${copyNumber==0?"":copyNumber}$format';
-          file= File(fileNameAndDirectory);
-          copyNumber++;
-          //delete file if exists
-          if (file.existsSync()) {
-            continue;
+        String newformat=videosInfo.first.format;
+        // String format=videosInfo.first.format;
+        if(videosInfo.first.format==".mp3"){
+          setState(() {
+            videosInfo.removeAt(0);
+            currentlyDownloading=video.title;
+            createListOfVideos("download function");
+          });
+          if (kDebugMode) {
+            print(video.title);
           }
-          break;
-        }
-        if (kDebugMode) {
-          print(file.path);
-        }
+          var manifest = await youtubeExplode.videos.streamsClient.getManifest(video.id);
+          var audio = manifest.audioOnly.withHighestBitrate();
+          var audioStream = youtubeExplode.videos.streamsClient.get(audio);
 
-        await file.create(recursive: true);
-        var output = file.openWrite(mode: FileMode.writeOnlyAppend);
-        var size = audio.size.totalBytes;
-        var count = 0;
-        await for (final data in audioStream) {
+          //time to create file
+          File file;
+          int copyNumber=0;
+          String fileNameAndDirectory;
+          while(true){
+            fileNameAndDirectory='$path/${video.title}${copyNumber==0?"":copyNumber}$newformat';
+            file= File(fileNameAndDirectory);
+            copyNumber++;
+            //delete file if exists
+            if (file.existsSync()) {
+              continue;
+            }
+            break;
+          }
+          if (kDebugMode) {
+            print(file.path);
+          }
+
+          await file.create(recursive: true);
+          var output = file.openWrite(mode: FileMode.writeOnlyAppend);
+          var size = audio.size.totalBytes;
+          var count = 0;
+          await for (final data in audioStream) {
             // Keep track of the current downloaded data.
             count += data.length;
             // Calculate the current progress.
@@ -315,22 +332,107 @@ class _DownloadScreenState extends State<DownloadScreen> {
             output.add(data);
 
 
+          }
+          if (kDebugMode) {
+            print("the end \n hold down your breath and couuuuuuuuunnnnnnt to ten \n feel the earth mooooove and then \n here my heart buuuuuurns \n agaaaaaaaain \n for this is the end");
+          }
+          output.close();
+          youtubeExplode.close();
+        }else{
+          String newformat=videosInfo.first.format;
+          setState(() {
+            videosInfo.removeAt(0);
+            currentlyDownloading=video.title;
+            createListOfVideos("download function");
+          });
+          if (kDebugMode) {
+            print(video.title);
+          }
+          var manifest = await youtubeExplode.videos.streamsClient.getManifest(video.id);
+          var audio = manifest.muxed.withHighestBitrate();
+          var audioStream = youtubeExplode.videos.streamsClient.get(audio);
+
+          //time to create file
+          File file;
+          int copyNumber=0;
+          String fileNameAndDirectory;
+          while(true){
+            fileNameAndDirectory='$path/${video.title}${copyNumber==0?"":copyNumber}$newformat';
+            file= File(fileNameAndDirectory);
+            copyNumber++;
+            //delete file if exists
+            if (file.existsSync()) {
+              continue;
+            }
+            break;
+          }
+          if (kDebugMode) {
+            print(file.path);
+          }
+
+          await file.create(recursive: true);
+          var output = file.openWrite(mode: FileMode.writeOnlyAppend);
+          var size = audio.size.totalBytes;
+          var count = 0;
+          await for (final data in audioStream) {
+            // Keep track of the current downloaded data.
+            count += data.length;
+            // Calculate the current progress.
+            double val=0;
+            val=((count/size));
+            var msg = ' Downloaded to\n $fileNameAndDirectory';
+            for (val; val == 1.0; val++) {
+              ScaffoldMessenger.of(context)
+                  .showSnackBar(SnackBar(content: Text(msg)));
+            }
+            setState(() => progress = val);
+            // Write to file.
+            output.add(data);
+
+
+          }
+          if (kDebugMode) {
+            print("the end \n hold down your breath and couuuuuuuuunnnnnnt to ten \n feel the earth mooooove and then \n here my heart buuuuuurns \n agaaaaaaaain \n for this is the end");
+          }
+          //todo: download video as mp3 and save mp3
+          await ImageGallerySaver.saveFile(fileNameAndDirectory, isReturnPathOfIOS: false);
+          // output.flush();
+
+          output.close();
+          youtubeExplode.close();
         }
-        if (kDebugMode) {
-          print("the end \n hold down your breath and couuuuuuuuunnnnnnt to ten \n feel the earth mooooove and then \n here my heart buuuuuurns \n agaaaaaaaain \n for this is the end");
-        }
-        //todo: download video as mp3 and save mp3
-        await ImageGallerySaver.saveFile(fileNameAndDirectory, isReturnPathOfIOS: false);
-        // output.flush();
-        output.close();
-        youtubeExplode.close();
+
+
 
       }
 
       }
-
-
-
     } //download function
+    void openDownloadFolder()async{
+      var permisson = await Permission.storage.request();
+      while(permisson.isDenied){
+        permisson = await Permission.storage.request();
+      }
+
+      if (permisson.isGranted) {
+
+        String path = await createFolderInAppDocDir();
+        if (kDebugMode) {
+          print(path);
+        }
+        try{
+          String value = await platform.invokeMethod("open",{
+            "path":path,
+          });
+        }catch(e){
+          if (kDebugMode) {
+            print(e);
+          }
+        }
+
+      }
+    }
+
+
   }
 
